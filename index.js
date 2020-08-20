d8c08160067defdd6cc8e54074f1b44d0bd6e04b
Epeli Ratoto <eratoto@yahoo.com>
2020-08-20 19:09:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
-function(string) {
+function sayHiGrandma(string) {
   if(string === string.toLowerCase()) {
     return "I can't hear you!";
   } else if(string === string.toUpperCase()) {
